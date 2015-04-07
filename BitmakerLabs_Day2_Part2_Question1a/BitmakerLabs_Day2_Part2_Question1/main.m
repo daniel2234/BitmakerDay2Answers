64e7e164ec587feffaa50b3b1b8ac0092672643a
Daniel Kwiatkowski <kwiatkowski.daniel@gmail.com>
2015-04-07 16:40:49 -0400

    changed folder name

---
+++ BitmakerLabs_Day2_Part2_Question1a/BitmakerLabs_Day2_Part2_Question1/main.m
@@ -0,0 +1,24 @@
+//
+//  main.m
+//  BitmakerLabs_Day2_Part2_Question1
+//
+//  Created by Daniel Kwiatkowski on 2015-04-07.
+//  Copyright (c) 2015 Daniel Kwiatkowski. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        int i;
+        i = 1;
+        NSLog(@"Testing...");
+        NSLog(@"....%i",i);
+        NSLog(@"...%i",i + 1);
+        NSLog(@"..%i", i + 2);
+    }
+    return 0;
+}
+
+
